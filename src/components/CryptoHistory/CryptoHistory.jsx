bcf19b1ff9930e6e0355685309feba8376c5409a
philips777 <mymailforall@ukr.net>
2023-08-27 16:16:23 +0300

    task4

--- src/components/CryptoHistory/CryptoHistory.jsx
+++ src/components/CryptoHistory/CryptoHistory.jsx
@@ -1,5 +1,28 @@
+import { formatDate } from 'Helpers';
 import { BaseTable, THead, Th, Tr, Td } from './CryptoHistory.styled';
 
-export const CryptoHistory = () => {
-  return <div>CryptoHistory</div>;
+export const CryptoHistory = ({ items }) => {
+  return (
+    <BaseTable>
+      <THead>
+        <tr>
+          <Th>№</Th>
+          <Th>PRICE</Th>
+          <Th>AMOUNT</Th>
+          <Th>DATE</Th>
+        </tr>
+      </THead>
+
+      <tbody>
+        {items.map((item, idx) => (
+          <Tr key={item.id}>
+            <Td>{idx + 1}</Td>
+            <Td>{item.price}</Td>
+            <Td>{item.amount}</Td>
+            <Td>{formatDate(item.date)}</Td>
+          </Tr>
+        ))}
+      </tbody>
+    </BaseTable>
+  );
 };
